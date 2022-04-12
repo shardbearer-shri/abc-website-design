6e958e09d818599f5a53c3198dfabc2ecc087f1f
shriniket <madefordotakeepout@gmail.com>
2022-04-13 01:45:52 +0530

    added responsiveness

---
+++ app.js
@@ -0,0 +1,15 @@
+const hamBtn = document.querySelector(".hamburger-icon");
+const navBar = document.querySelector(".navigation");
+const allNavLink = document.querySelectorAll(".nav__link");
+
+hamBtn.addEventListener("click", () => {
+  navBar.classList.toggle("hamTog");
+  hamBtn.classList.toggle("hamburger-icon-hover");
+});
+
+allNavLink.forEach((ele) => {
+  ele.addEventListener("click", () => {
+    navBar.classList.toggle("hamTog");
+    hamBtn.classList.toggle("hamburger-icon-hover");
+  });
+});
